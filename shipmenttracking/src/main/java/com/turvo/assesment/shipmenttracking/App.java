d1fd01e7fb1deee07af794336c0da1b4d11e840c
MndSandeep <sandeepallamsetti@gmail.com>
2017-08-24 10:22:18 +0530

    JMS changes

--- shipmenttracking/src/main/java/com/turvo/assesment/shipmenttracking/App.java
+++ shipmenttracking/src/main/java/com/turvo/assesment/shipmenttracking/App.java
@@ -7,19 +7,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jms.annotation.EnableJms;
 
 /**
- * Main application class which initializes all the required
- * configuration & boot the application
+ * Main application class which initializes all the required configuration &
+ * boot the application
+ * 
  * @author Sandeep Allamsetti
  */
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableJms
 @EnableJpaRepositories
-public class App 
-{
-	
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(App.class, args);
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES",
+				"java.lang, javax.security, java.util, org.apache.activemq, org.fusesource.hawtbuf, com.thoughtworks.xstream.mapper,com.turvo.assesment.shipmenttracking,com.turvo.assesment.shipmenttracking.notification");
+		SpringApplication.run(App.class, args);
+	}
 }
